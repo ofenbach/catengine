3b9d5383e96e286b5cc0ee66838b104833fac97a
Tim Ofenbach <t.ofenbach@web.de>
2018-04-23 21:41:53 +0200

    Create Camera.java
    Camera file to change current screen (e.g. switch from intro to menu).

---
+++ main/Camera.java
@@ -0,0 +1,11 @@
+package catgames.main;    // TODO: Change this one to your main
+
+public class Camera {
+
+    public static int currentscreen_id = 0;
+
+    public static void setScreen(int id) {
+        currentscreen_id = id;
+    }
+
+}
