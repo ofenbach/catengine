764a4efcfec1e1800dc0639831f7cf6367159cd7
Tim Ofenbach <t.ofenbach@web.de>
2018-04-26 02:13:45 +0200

    Update Camera.java
    added explanation

--- engine/main/Camera.java
+++ engine/main/Camera.java
@@ -2,15 +2,19 @@ package ofenbach.exampleapp.main;	// change this to your package name
 
 /* to switch screens            */
 /* @creation:   March 2018      */
-/* @update:     25.4.2018       */
+/* @update:     26.4.2018       */
 /* @author:     Tim B. Ofenbach */
 
 public class Camera {
 
-    public static int currentscreen_id = 0;
+    /* Explanation: */
+    /* only saves which screen_id to display */
+    /* MainView displays Screen.children[Camera.currentscreen_id] */
+    
+    public static int currentscreen_id = 0;     // current screen to display
 
     public static void setScreen(int id) {
-        currentscreen_id = id;
+        currentscreen_id = id;      // switch current screen to display
     }
 
 }
