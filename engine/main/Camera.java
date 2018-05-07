c12dcf00c7e198ae0d83e4653d5d0e2824061e0e
Tim B. Ofenbach <t.ofenbach@web.de>
2018-05-08 01:46:46 +0200

    Update Camera.java
    improved code

--- engine/main/Camera.java
+++ engine/main/Camera.java
@@ -5,16 +5,16 @@ package ofenbach.exampleapp.main;	// change this to your package name
 /* @update:     26.4.2018       */
 /* @author:     Tim B. Ofenbach */
 
-public class Camera {
+/* Explanation: */
+/* only saves which screen_id to display */
+/* MainView displays Screen.children[Camera.currentscreen_id] */
 
-    /* Explanation: */
-    /* only saves which screen_id to display */
-    /* MainView displays Screen.children[Camera.currentscreen_id] */
+public class Camera {
     
-    public static int currentscreen_id = 0;     // current screen to display
+    public static int current_screen_id = 0;     // current screen to display
 
     public static void setScreen(int id) {
-        currentscreen_id = id;      // switch current screen to display
+        current_screen_id = id;      // switch current screen to display
     }
 
 }
