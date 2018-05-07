bef8c3796281125146bf11775e47675f61bd4cd3
Tim B. Ofenbach <t.ofenbach@web.de>
2018-05-08 01:48:58 +0200

    Update Screen.java
    improved code

--- engine/main/Screen.java
+++ engine/main/Screen.java
@@ -8,12 +8,12 @@ import java.util.List;
 /* @update:     25.4.2018       */
 /* @author:     Tim B. Ofenbach */
 
+/* Explanation: */
+/* acts as a superclass for your screens. */
+/* here are all the screens which were created stored  */
+/* MainView displays the right screen (id stored at Camera) via this list */
+
 public class Screen  {
-    
-    /* Explanation: */
-    /* acts as a superclass for your screens. */
-    /* here are all the screens which were created stored  */
-    /* MainView displays the right screen (id stored at Camera) via this list */
 
     public static List<Screen> screens = new ArrayList<>(); // child list (used by MainView to display the right screen)
 
@@ -21,14 +21,11 @@ public class Screen  {
         screens.add(this);  // save child
     }
 
+    /* @override */
     public void repeat() {} // loop 60FPS
-
     public void load() {}   // when the screen starts
-
     public void touchHold() {}  // triggered by touch
-
     public void touchDown() {}
-
     public void touchUp() { }
 
 }
