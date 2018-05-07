2fc723633bf1db03b33753e816855f7efc1373d1
Tim B. Ofenbach <t.ofenbach@web.de>
2018-05-08 01:47:42 +0200

    Update MainView.java
    improved code

--- engine/main/MainView.java
+++ engine/main/MainView.java
@@ -11,13 +11,12 @@ import ofenbach.exampleapp.views.GameStart;	// change this to your package name
 /* @update:     28.4.2018       */
 /* @author:     Tim B. Ofenbach */
 
-public class MainView extends View {
+/* Explanation: */
+/* This Class is the real screen thats being displayed by android os. */
+/* It selects the current screen (saved in Camera) and repeats it's repeat() method. */
 
-    /** ONLY CHANGE THIS IF YOU KNOW WHAT YOU ARE DOING! **/
-    
-    /* Explanation: */
-    /* This Class is the real screen thats being displayed by android os. */
-    /* It selects the current screen (saved in Camera) and repeats it's repeat() method. */
+/** DO NOT CHANGE THIS UNLESS YOU KNOW WHAT YOU ARE DOING! **/
+public class MainView extends View {
 
     public static Canvas global_canvas;     // used by Image.java to draw
 
@@ -44,10 +43,10 @@ public class MainView extends View {
             gamestart.start();
         }
 
-        Screen.screens.get(Camera.currentscreen_id).repeat();   // display current screen
+        Screen.screens.get(Camera.current_screen_id).repeat();   // display current screen
 
         if (touch_hold) {
-            Screen.screens.get(Camera.currentscreen_id).touchHold();    // trigger touch function in the right screen
+            Screen.screens.get(Camera.current_screen_id).touchHold();    // trigger touch function in the right screen
         }
 
         invalidate();   // repeat this method
@@ -61,12 +60,12 @@ public class MainView extends View {
 
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {    // finger starts touching the screen
             touch_hold = true;
-            Screen.screens.get(Camera.currentscreen_id).touchDown();
+            Screen.screens.get(Camera.current_screen_id).touchDown();
         }
 
         if (ev.getAction() == MotionEvent.ACTION_UP) {  // finger loses touch
             touch_hold = false;
-            Screen.screens.get(Camera.currentscreen_id).touchUp();
+            Screen.screens.get(Camera.current_screen_id).touchUp();
         }
 
         return true;
